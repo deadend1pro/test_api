442eccdfae87214334da82dd9838c0f36e661b28
Luke Walsh <luke.walshgt@gmail.com>
2024-03-13 15:13:44 +0100

    apikey

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
 const app = express();
 const environment = process.env.NODE_ENV;
 
@@ -18,6 +19,13 @@ app.get("/greeting", (req, res) => {
   res.json({ value: greetings[greetingsindex] });
 });
 
+app.get("/game/:today", async (req, res) => {
+  const response = await fetch(
+    `https://rawg.io/@breathnach/apikey=${req.params.location}&apikey=${API_KEY}`
+  );
+  const data = await response.json();
+  res.send("Current GAME: " + data.timelines.minutely[0].values);
+});
 app.listen(PORT, () => {
   if (environment === "development") {
     console.log(`The server is listening on http://localhost:${PORT}`);
