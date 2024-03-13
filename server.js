2c650995d77cb8f56669d0d3c85f109a15b2ee4f
Luke Walsh <luke.walshgt@gmail.com>
2024-03-13 13:40:56 +0100

    greeting

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get("/random-number", (req, res) => {
     const number = Math.floor(Math.random() * 100)
     res.json({ value: number })
 })
+
+app.get("/greeting", (req, res) => {
+    const greetings = ["hey", "hi", "dia duit", "hola"];
+    const greetingsindex = Math.floor(Math.random() * greetings.length)
+    res.json({ value: greetings[greetingsindex] })
+})
 app.listen(PORT, () => {
     console.log(`The server is listening on http://localhost:${PORT}`);
   });
