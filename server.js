be29d4077fe4d0efd7748db69bf568232ba21b85
Luke Walsh <luke.walshgt@gmail.com>
2024-03-13 13:30:17 +0100

    start

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 })
 app.get("/random-number", (req, res) => {
     const number = Math.floor(Math.random() * 100)
-    res.json(number)
+    res.json({ value: number })
 })
 app.listen(PORT, () => {
     console.log(`The server is listening on http://localhost:${PORT}`);
