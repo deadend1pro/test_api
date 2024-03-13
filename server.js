885d70d4ea53e385d80847c57113289de27f6021
Luke Walsh <luke.walshgt@gmail.com>
2024-03-13 14:05:30 +0100

    env

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-import express from "express"
+import express from "express";
+
 
 const PORT = 3000;
 const app = express();
 
+
 app.get("/", (req, res) => {
-    res.json("welcome")
-})
+  res.json("welcome");
+});
 app.get("/random-number", (req, res) => {
-    const number = Math.floor(Math.random() * 100)
-    res.json({ value: number })
-})
+  const number = Math.floor(Math.random() * 100);
+  res.json({ value: number });
+});
 
 app.get("/greeting", (req, res) => {
-    const greetings = ["hey", "hi", "dia duit", "hola"];
-    const greetingsindex = Math.floor(Math.random() * greetings.length)
-    res.json({ value: greetings[greetingsindex] })
-})
+  const greetings = ["hey", "hi", "dia duit", "hola"];
+  const greetingsindex = Math.floor(Math.random() * greetings.length);
+  res.json({ value: greetings[greetingsindex] });
+});
+
 app.listen(PORT, () => {
-    console.log(`The server is listening on http://localhost:${PORT}`);
-  });
+  console.log(`The server is listening on http://localhost:${PORT}`);
+});
